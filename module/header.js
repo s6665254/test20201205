8a7ff7899b87cb2e5856b47f24d816b126e64e74
s6665254 <74740474+s6665254@users.noreply.github.com>
2020-12-05 20:24:57 +0800

    Add files via upload

--- module/header.js
+++ module/header.js
@@ -1,4 +1,4 @@
 var StrVar="";
-StrVar+='<div style="font-size:30px; text-align:center"; >好的交易</div>';
-StrVar+='<div style="font-size:10px; text-align:center"; >還在看沒有量化的新聞資訊，你需要來點經濟學</div>';
+StrVar+='<div style="font-size:30px; text-align:center;line-height:1.2" >好的交易</div>';
+StrVar+='<div style="font-size:10px; text-align:center"; >還在荒野蒐集碎片的資訊? 你值得擁有更好的交易</div>';
 $('#header').append(StrVar);
